9ba3b60ac61f0f709dfa26cc940dcfae2d129e23
Lucas <lucasarnoldd@gmail.com>
2024-01-01 14:40:05 -0300

    create Server, Middleware, Utils and Routes

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import { buildRoutePath } from './utils/build-route-path.js';
+
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+
+    }
+  },
+];
