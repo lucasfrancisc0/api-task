47a3afc6c2816f919e64c46fb97fa1e38726f0b6
Lucas <lucasarnoldd@gmail.com>
2024-01-02 14:29:49 -0300

    Finish routes and insert method update, delete in database

--- src/database.js
+++ src/database.js
@@ -45,9 +45,23 @@ export class Database {
     return data;
   };
 
+  update(table, id, data){
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
 
+    if(rowIndex > -1){
+      const row = this.#database[table][rowIndex]
+      this.#database[table][rowIndex] = { id, ...row, ...data }
+      this.#persist()
+    }
 
+  };
 
-  
+  delete(table, id){
+    const rowIndex = this.#database[table].findIndex(row => row.id === id)
 
+    if(rowIndex > -1){
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  };
 }
